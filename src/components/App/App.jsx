26898f643d8e332851a9af7cc13f2ebc697a8d0e
Malanchenko Sergii <malan.sergii@gmail.com>
2022-02-09 11:12:50 +0200

    add table and list

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import BasicTable from "../BasicTable/BasicTable";
+import UsersLists from "../UsersList/UsersList";
+export const App = () => {
+  return (
+    <>
+      <BasicTable />
+      <UsersLists />
+    </>
+  );
+};
+
+
+// https://api.instantwebtools.net/v1/passenger?&size=10 
