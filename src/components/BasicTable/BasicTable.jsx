5ac7d10c99176304d35ab776a44bdfbd796f54b6
Malanchenko Sergii <malan.sergii@gmail.com>
2022-02-10 11:14:13 +0200

    table create

--- src/components/BasicTable/BasicTable.jsx
+++ src/components/BasicTable/BasicTable.jsx
@@ -6,18 +6,11 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import passangers from "../../data/passengers.json";
+import Avatar from '@mui/material/Avatar';
+import Link from '@mui/material/Link';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 export default function BasicTable() {
   return (
@@ -25,26 +18,30 @@ export default function BasicTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>airline name</TableCell>
+            <TableCell >logo</TableCell>
+            <TableCell >name</TableCell>
+            <TableCell >slogan</TableCell>
+            <TableCell >website</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {passangers.map((passanger) => (
             <TableRow
-              key={row.name}
+              key={passanger._id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {passanger.airline.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+                  <TableCell >
+                       <Avatar alt={passanger.airline.name} src={passanger.airline.logo} />
+                   </TableCell>
+              <TableCell >{passanger.name}</TableCell>
+              <TableCell >{passanger.airline.slogan}</TableCell>
+                  <TableCell >
+                      <Link href={passanger.airline.website}>{passanger.airline.website}</Link>
+                  </TableCell>
             </TableRow>
           ))}
         </TableBody>
