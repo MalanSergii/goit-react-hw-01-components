0e9461a2482ae1e78476fd6b174289787e868af4
Malanchenko Sergii <malan.sergii@gmail.com>
2022-02-10 12:08:44 +0200

    0tol

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,9 @@
-import BasicTable from "../BasicTable/BasicTable";
-import UsersLists from "../UsersList/UsersList";
-import { Section , Container} from "./App.styled";
+
+
 
 export const App = () => {
   return (
-    <Container>
-      <Section>
-        <BasicTable />
-      </Section>
-      <Section>
-        <UsersLists />
-      </Section>
-    </Container>
+    <></>
   );
 };
 
