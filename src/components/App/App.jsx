f9143daafdf6a028f0ed58223bc44ab8db12523b
Malanchenko Sergii <malan.sergii@gmail.com>
2022-02-12 15:49:14 +0200

    add 5 props to 1-st exercice

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,17 +3,23 @@ import Statistics from "components/Statistics";
 import FriendsList from "components/FriendsList";
 import TransactionHistory from "components/TransactionHistory"
 
-import userData from "../Data/userData.json";
-import statisticsData from "../Data/statisticsData.json";
-import friends from "../Data/friends.json";
-import transactions from "../Data/transactions.json"
+import userData from "../../Data/userData.json";
+import statisticsData from "../../Data/statisticsData.json";
+import friends from "../../Data/friends.json";
+import transactions from "../../Data/transactions.json"
 
 import { Container } from "./App.styled";
 
 export const App = () => {
   return (
     <Container>
-      <Profile {...userData}/>
+      <Profile
+        userName={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
       <Statistics title="Upload stats" stats={statisticsData} />
       <FriendsList friends={friends} />
       <TransactionHistory history={transactions}/>
